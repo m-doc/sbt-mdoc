1fa0462571cb15db2b24b4d9ad550d8a7098b9c6
Frank S. Thomas <frank@timepit.eu>
2016-01-27 23:27:49 +0100

    More defaults

--- src/main/scala/org/mdoc/MdocPlugin.scala
+++ src/main/scala/org/mdoc/MdocPlugin.scala
@@ -8,8 +8,10 @@ object MdocPlugin extends AutoPlugin {
   override def requires = bintray.BintrayPlugin
 
   override lazy val projectSettings = Seq(
-    Keys.organization := "org.m-doc",
+    Keys.homepage := Some(sbt.url("https://github.com/m-doc/" + Keys.name.value)),
     Keys.licenses += "Apache-2.0" -> sbt.url("http://www.apache.org/licenses/LICENSE-2.0"),
+    Keys.organization := "org.m-doc",
+    Keys.publishMavenStyle := true,
     BintrayKeys.bintrayOrganization := Some("m-doc")
   )
 }
