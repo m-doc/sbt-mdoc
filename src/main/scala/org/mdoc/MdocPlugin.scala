830c29f414b4b705b2db9272e1b71fdbeba0e51d
Frank S. Thomas <frank@timepit.eu>
2016-01-28 05:30:02 +0100

    We want GitVersioning

--- src/main/scala/org/mdoc/MdocPlugin.scala
+++ src/main/scala/org/mdoc/MdocPlugin.scala
@@ -1,13 +1,13 @@
 package org.mdoc
 
 import bintray.{ BintrayKeys, BintrayPlugin }
-import com.typesafe.sbt.GitPlugin
+import com.typesafe.sbt.GitVersioning
 import com.typesafe.sbt.SbtGit.GitKeys
 import sbt.{ AutoPlugin, Keys }
 
 object MdocPlugin extends AutoPlugin {
 
-  override def requires = BintrayPlugin && GitPlugin
+  override def requires = BintrayPlugin && GitVersioning
 
   override lazy val projectSettings = Seq(
     Keys.homepage := Some(sbt.url("https://github.com/m-doc/" + Keys.name.value)),
