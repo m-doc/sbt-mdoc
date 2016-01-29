688411b339d6adcde3c9b84d3873dfb588af7e68
Frank S. Thomas <frank@timepit.eu>
2016-01-29 06:56:55 +0100

    Set scalaVersion and scmInfo

--- src/main/scala/org/mdoc/MdocPlugin.scala
+++ src/main/scala/org/mdoc/MdocPlugin.scala
@@ -3,15 +3,15 @@ package org.mdoc
 import bintray.{ BintrayKeys, BintrayPlugin }
 import com.typesafe.sbt.SbtGit.git
 import com.typesafe.sbt.{ GitVersioning, SbtScalariform }
-import sbt.{ AutoPlugin, Keys }
+import sbt._
 
 object MdocPlugin extends AutoPlugin {
 
   override def requires = BintrayPlugin && GitVersioning && SbtScalariform
 
   override lazy val projectSettings = Seq(
-    Keys.homepage := Some(sbt.url("https://github.com/m-doc/" + Keys.name.value)),
-    Keys.licenses += "Apache-2.0" -> sbt.url("http://www.apache.org/licenses/LICENSE-2.0"),
+    Keys.homepage := Some(githubUrl(Keys.name.value)),
+    Keys.licenses += "Apache-2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0"),
     Keys.organization := "org.m-doc",
     Keys.publishMavenStyle := true,
     Keys.scalacOptions ++= Seq(
@@ -29,7 +29,22 @@ object MdocPlugin extends AutoPlugin {
       "-Ywarn-numeric-widen",
       "-Ywarn-value-discard"
     ),
+    Keys.scalaVersion := "2.11.7",
+    Keys.scmInfo := Some(ScmInfo(
+      githubUrl(Keys.name.value),
+      s"scm:git:${gitUrl(Keys.name.value)}",
+      Some(s"scm:git:${gitDevUrl(Keys.name.value)}")
+    )),
     BintrayKeys.bintrayOrganization := Some("m-doc"),
     git.useGitDescribe := true
   )
+
+  def githubUrl(projectName: String): URL =
+    url(s"https://github.com/m-doc/$projectName")
+
+  def gitUrl(projectName: String): String =
+    s"https://github.com/m-doc/$projectName.git"
+
+  def gitDevUrl(projectName: String): String =
+    s"git@github.com:m-doc/$projectName.git"
 }
