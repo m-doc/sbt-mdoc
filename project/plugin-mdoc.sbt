8f281a19f0b69ec489572c14fdbcf3992b3224e0
Frank S. Thomas <frank@timepit.eu>
2016-02-01 04:32:48 +0100

    Create bintray credentials in ~

--- project/plugin-mdoc.sbt
+++ project/plugin-mdoc.sbt
@@ -1,4 +1,4 @@
 resolvers += Resolver.url("m-doc/sbt-plugins",
   url("http://dl.bintray.com/content/m-doc/sbt-plugins"))(Resolver.ivyStylePatterns)
 
-addSbtPlugin("org.m-doc" % "sbt-mdoc" % "0.0.0-25-g66ec984")
+addSbtPlugin("org.m-doc" % "sbt-mdoc" % "0.0.0-27-g7f3b73e")
--- src/main/scala/org/mdoc/sbtmdoc/MdocPlugin.scala
+++ src/main/scala/org/mdoc/sbtmdoc/MdocPlugin.scala
@@ -36,7 +36,7 @@ object MdocPlugin extends AutoPlugin {
 
   override lazy val projectSettings = Seq(
     makeBintrayCredentials := {
-      val credentials = file(".bintray/.credentials")
+      val credentials = file(Path.userHome.absolutePath + "/.bintray/.credentials")
       val user = Option(System.getenv("BINTRAY_USER")).getOrElse("")
       val pass = Option(System.getenv("BINTRAY_PASSWORD")).getOrElse("")
       if (!credentials.exists()) {
