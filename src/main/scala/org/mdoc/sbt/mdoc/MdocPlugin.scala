76c210ab9dfc766fe02076cd353781de34bb9260
Frank S. Thomas <frank@timepit.eu>
2016-02-05 05:13:20 +0100

    Add scala-logging version

--- src/main/scala/org/mdoc/sbt/mdoc/MdocPlugin.scala
+++ src/main/scala/org/mdoc/sbt/mdoc/MdocPlugin.scala
@@ -22,6 +22,7 @@ object MdocPlugin extends AutoPlugin {
       val logback = "1.1.3"
       val http4s = "0.12.1"
       val properly = "0.0.0-18-g5fef5a3"
+      val scalaLogging = "3.1.0"
       val scalacheck = "1.13.0"
       val scalaz = "7.1.6"
       val scodecBits = "1.0.12"
