b316bb1cd1c632eeeca5562d5155848fcfbc78fe
Frank S. Thomas <f.thomas@gmx.de>
2016-02-06 16:14:32 +0100

    Add circe-core

--- src/main/scala/org/mdoc/sbt/mdoc/Library.scala
+++ src/main/scala/org/mdoc/sbt/mdoc/Library.scala
@@ -7,6 +7,7 @@ object Library {
   val fshell = "org.m-doc" %% "fshell" % Version.fshell
   val renderingEngines = "org.m-doc" %% "rendering-engines" % Version.renderingEngines
 
+  val circeCore = "io.circe" %% "circe-core" % Version.circe
   val circeGeneric = "io.circe" %% "circe-generic" % Version.circe
   val http4sCirce = "org.http4s" %% "http4s-circe" % Version.http4s
   val http4sCore = "org.http4s" %% "http4s-core" % Version.http4s
