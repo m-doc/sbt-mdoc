a40ed768a4a8359dcbc42fe40a5571acdb431b44
Frank S. Thomas <frank@timepit.eu>
2016-02-13 16:46:56 +0100

    Add scalastyle-sbt-plugin as dependency

--- build.sbt
+++ build.sbt
@@ -9,6 +9,7 @@ addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.5.0")
 addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "0.8.5")
 addSbtPlugin("me.lessis" % "bintray-sbt" % "0.3.0")
 addSbtPlugin("org.scalariform" % "sbt-scalariform" % "1.6.0")
+addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.8.0")
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.3.5")
 
 publishMavenStyle := false
--- project/MdocLibrary.scala
+++ project/MdocLibrary.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object MdocLibrary {
-  val fshell = "org.m-doc" %% "fshell" % "0.0.0-35-gc069fae"
-  val renderingEngines = "org.m-doc" %% "rendering-engines" % "0.0.0-16-g837ff08"
-  val commonModel = "org.m-doc" %% "common-model" % "0.0.0-7-g1831dc0"
+  val fshell = "org.m-doc" %% "fshell" % "0.0.0-38-gf528291"
+  val renderingEngines = "org.m-doc" %% "rendering-engines" % "0.0.0-17-g088fa18"
+  val commonModel = "org.m-doc" %% "common-model" % "0.0.0-10-g73865cc"
 }
