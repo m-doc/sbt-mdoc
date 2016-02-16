a5450d3b898cb091a9756f6f38b7256137816707
Frank S. Thomas <frank@timepit.eu>
2016-02-16 17:53:24 +0100

    Do not set serverLoading

--- src/main/scala/org/mdoc/sbt/mdoc/ExtNativePackager.scala
+++ src/main/scala/org/mdoc/sbt/mdoc/ExtNativePackager.scala
@@ -1,15 +1,12 @@
 package org.mdoc.sbt.mdoc
 
 import com.typesafe.sbt.packager
-import com.typesafe.sbt.SbtNativePackager
 import sbt.Keys
 
 object ExtNativePackager {
   lazy val settings = Seq(
     packager.Keys.maintainer := "m-doc <info@m-doc.org>",
     packager.Keys.packageDescription := s"See <${Keys.homepage.value.getOrElse("")}> for more information.",
-    packager.Keys.packageSummary := Keys.description.value,
-    packager.Keys.serverLoading in SbtNativePackager.Debian :=
-      com.typesafe.sbt.packager.archetypes.ServerLoader.SystemV
+    packager.Keys.packageSummary := Keys.description.value
   )
 }
