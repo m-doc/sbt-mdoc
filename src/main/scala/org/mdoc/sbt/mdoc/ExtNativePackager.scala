9ac067e39abc86bdbfd989ab84ab9650a3b38e70
Frank S. Thomas <frank@timepit.eu>
2016-02-18 21:18:53 +0100

    Prefix debian packages with mdoc-

--- src/main/scala/org/mdoc/sbt/mdoc/ExtNativePackager.scala
+++ src/main/scala/org/mdoc/sbt/mdoc/ExtNativePackager.scala
@@ -1,11 +1,18 @@
 package org.mdoc.sbt.mdoc
 
 import com.typesafe.sbt.packager
+import com.typesafe.sbt.packager.debian.DebianPlugin.autoImport.Debian
+import com.typesafe.sbt.packager.linux.LinuxPlugin.autoImport.Linux
 import sbt.Keys
 
 object ExtNativePackager {
   lazy val settings = Seq(
+    Keys.name in Debian := s"mdoc-${Keys.name.value}",
+    Keys.name in Linux := Keys.name.in(Debian).value,
+    packager.Keys.executableScriptName := Keys.name.in(Debian).value,
     packager.Keys.maintainer := "m-doc <info@m-doc.org>",
+    packager.Keys.packageName in Debian := Keys.name.in(Debian).value,
+    packager.Keys.packageName in Linux := Keys.name.in(Debian).value,
     packager.Keys.packageDescription := s"See <${Keys.homepage.value.getOrElse("")}> for more information.",
     packager.Keys.packageSummary := Keys.description.value
   )
