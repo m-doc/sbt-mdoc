84f933496f2ded14d53d7d4f804c9333c7d0b186
Frank S. Thomas <frank@timepit.eu>
2016-02-26 20:38:00 +0100

    Update http4s

--- src/main/scala/org/mdoc/sbt/mdoc/Version.scala
+++ src/main/scala/org/mdoc/sbt/mdoc/Version.scala
@@ -3,7 +3,7 @@ package org.mdoc.sbt.mdoc
 object Version {
   val bootstrap = "3.3.6"
   val circe = "0.2.0"
-  val http4s = "0.12.1"
+  val http4s = "0.12.3"
   val jquery = "2.2.0"
   val logback = "1.1.3"
   val properly = "0.0.0-18-g5fef5a3"
