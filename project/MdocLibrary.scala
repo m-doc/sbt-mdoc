4b7f596bae2182d0d75e24635b2dd21060287e95
Frank S. Thomas <frank@timepit.eu>
2016-03-13 13:04:44 +0100

    Add sbt-jolokia

--- project/MdocLibrary.scala
+++ project/MdocLibrary.scala
@@ -3,5 +3,5 @@ import sbt._
 object MdocLibrary {
   val fshell = "org.m-doc" %% "fshell" % "0.0.0-51-gaa54d69"
   val renderingEngines = "org.m-doc" %% "rendering-engines" % "0.0.0-27-g1caff3f"
-  val commonModel = "org.m-doc" %% "common-model" % "0.0.0-22-g701f7a7"
+  val commonModel = "org.m-doc" %% "common-model" % "0.0.0-26-g0c76b40"
 }
--- project/plugin-mdoc.sbt
+++ project/plugin-mdoc.sbt
@@ -1,4 +1,4 @@
 resolvers += Resolver.url("m-doc/sbt-plugins",
   url("http://dl.bintray.com/content/m-doc/sbt-plugins"))(Resolver.ivyStylePatterns)
 
-addSbtPlugin("org.m-doc" % "sbt-mdoc" % "0.0.0-85-ge46fbd5")
+addSbtPlugin("org.m-doc" % "sbt-mdoc" % "0.0.0-86-g7dc6eee")
